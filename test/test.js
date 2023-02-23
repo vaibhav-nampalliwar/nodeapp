f33cfa033230bde5ece6d66d6d4de9f48adc5465
vaibhav <vaibhavnampalliwar@gmail.com>
2023-02-23 19:25:48 +0530

    eight

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
  it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world');
+ request(app).get('/').expect('hello world', done);
  });
 });
